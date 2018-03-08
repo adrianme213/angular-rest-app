7a85c5882694e3acfa6a96352213eb311c9f2316
Adrian Meza <adrianme213@gmail.com>
2018-03-07 22:13:17 -0800

    [feat] - style widgets component

--- src/app/widgets/widgets.component.ts
+++ src/app/widgets/widgets.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WidgetsComponent implements OnInit {
   selectedWidget;
+
   widgets: {id: number, name: string, description: string}[] = [
     {
       id: 1,
